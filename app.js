32aa50a48bb02577868465c8d298bd118317b999
Mark Leung <mark.leung89@gmail.com>
2020-11-06 23:49:36 +1100

    Latest changes

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
+const passport = require('passport')
+const session = require('express-session')
 
 // Load config
 dotenv.config({path: './config/config.env'})
 
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
 const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine','.hbs');
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialise())
+app.use(passport.session())
+
+// Static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+
 
 const PORT = process.env.PORT || 3000
 
